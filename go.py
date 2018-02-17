d3a77c6976573414502225922dd43629980289ac
zdimon <zdimon77@gmail.com>
2018-02-17 20:19:16 +0300

    first

---
+++ go.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+print 'Hello world!!!'
+
+dictionary = { '2': 'text 1', '4': 'text' }
+
+print dictionary['age']
+
+'''
+for i in [2,4,5,6,7,8]:
+    filename = '%s.txt' % i 
+    f = open(filename,'r')
+    #f.write('asdaasfsafasfass')
+    print f.read()
+    f.close()
+    if i>5:
+        print '%s greater than 5' % i
+    else:
+        print '%s less than 5' % i
+    #print 'current value is %s' % i
+    #print int('22')+i    
+
+
+print 'End program'
+'''    
